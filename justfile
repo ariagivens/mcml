1ee71d421af7daabcdfa9a5a411ae544e79372f3
Aria Givens <aria@ariagivens.com>
2024-01-24 23:16:03 +0000

    Commandline compilation

--- justfile
+++
@@ -1,3 +0,0 @@
-test:
-    cargo run -- tests/literals.mcml -o /tmp/literals
-    cd ../mctest && cargo run --release -- /tmp/literals
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use mcml::compile;
+use std::path::PathBuf;
+use std::fs;
+use anyhow::Result;
+
+#[derive(Parser)]
+struct Args {
+    input: PathBuf,
+    output: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let Args { input, output } = Args::parse();
+    let source = fs::read_to_string(input)?;
+    let datapack = compile(&source)?;
+    fs::write(output, datapack.bytes()?)?;
+
+    Ok(())
 }
